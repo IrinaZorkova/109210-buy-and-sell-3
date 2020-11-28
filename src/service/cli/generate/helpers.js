f9cfd5c54aa83496b36559b8eeff32d6c6c2b231
IrinaZorkova <irinanazorkova@gmail.com>
2020-11-28 14:42:12 +0300

    merge

--- src/service/cli/generate/helpers.js
+++ src/service/cli/generate/helpers.js
@@ -3,26 +3,26 @@
 const fs = require(`fs`);
 const {FILE_NAME} = require(`./constants`);
 
-module.exports.getRandomInt = (_min, _max) => {
-  const min = Math.ceil(_min);
-  const max = Math.floor(_max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+module.exports.getRandomInt = (min, max) => {
+  const _min = Math.ceil(min);
+  const _max = Math.floor(max);
+  return Math.floor(Math.random() * (_max - _min + 1)) + _min;
 };
 
-module.exports.shuffle = (_array) => {
-  const array = [..._array];
+module.exports.shuffle = (array) => {
+  const _array = [...array];
 
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = _array.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
-    [array[i], array[randomPosition]] = [array[randomPosition], array[i]];
+    [_array[i], _array[randomPosition]] = [_array[randomPosition], _array[i]];
   }
 
   return array;
 };
 
-module.exports.getPictureFileName = (_id) => {
-  const id = _id < 10 ? `0${_id}` : _id;
-  return `item${id}.jpg`;
+module.exports.getPictureFileName = (id) => {
+  const _id = id < 10 ? `0${id}` : id;
+  return `item${_id}.jpg`;
 };
 
 module.exports.writeFile = (content) => {
