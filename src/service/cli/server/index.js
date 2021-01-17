6b6b6e3399aa9e9bd28e23a4cacb7c1db226bc87
IrinaZorkova <irinanazorkova@gmail.com>
2021-01-17 21:54:19 +0300

    cli server with express

--- src/service/cli/server/index.js
+++ src/service/cli/server/index.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const chalk = require(`chalk`);
-const http = require(`http`);
-const {onClientConnect} = require(`./onClientConnect`);
+const {offersController} = require(`./offers.controller`);
+const {Router} = require(`express`);
+const router = new Router();
 const {DEFAULT_PORT} = require(`../../../constants`);
+
+const express = require(`express`);
+
+const app = express();
+
+router.get(`/offers`, offersController);
 
 module.exports = {
   name: `--server`,
@@ -11,15 +17,12 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(`Ошибка при создании сервера`, err);
-        }
+    app.listen(port, () => {
+      console.log(`Сервер принимает подключения на ${port}`);
+    });
 
-        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      });
+    app.use(express.json());
 
+    app.use(router);
   }
 };
